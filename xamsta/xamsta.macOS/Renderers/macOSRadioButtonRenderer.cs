93af6e671ef738e7156b211a0d5b1df84e4d0984
MH Rastegari <42671084+mhrastegary77@users.noreply.github.com>
2021-05-09 11:38:59 +0430

    Added macOSRadioButtonRenderer

---
+++ xamsta/xamsta.macOS/Renderers/macOSRadioButtonRenderer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using AppKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.MacOS;
+
+[assembly: ExportRenderer(typeof(RadioButton), typeof(xamsta.macOS.Renderers.macOSRadioButtonRenderer))]
+namespace xamsta.macOS.Renderers
+{
+	public class macOSRadioButtonRenderer : RadioButtonRenderer
+	{
+		void HandleActivated(object sender, EventArgs args)
+		{
+			if (Element == null || sender == null)
+				return;
+			
+			Element.IsChecked = (sender as NSButton).State == NSCellStateValue.On;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (Control != null)
+				Control.Activated -= HandleActivated;
+
+			base.Dispose(disposing);
+		}
+
+		protected override void OnElementChanged(ElementChangedEventArgs<RadioButton> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control != null)
+				Control.Activated += HandleActivated;
+		}
+	}
+}
